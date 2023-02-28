0db4a0a7a69a934c6cb3135db9dffd36bec9c854
KarthikC21 <126549886+KarthikC21@users.noreply.github.com>
2023-02-28 17:03:00 +0530

    Add files via upload
    Test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import react from 'react';
+import Main from './components/Main';
+import './components/style.css';
+
+function App() {
+  return (
+   <>
+      <Main/>
+   </>
+
+  );
+}
+
+export default App;
